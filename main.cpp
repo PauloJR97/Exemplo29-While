5967be60a619d33ebd7f2a4f36ce4229d764ab3c
Paulo Junior <>
2021-04-24 20:18:24 +0000

    Feito Do While

--- main.cpp
+++ main.cpp
@@ -5,17 +5,16 @@ int main()
  {
    int n = 0, maior = 0;
    
-   cout<<"Informe o valor de n: ";
-   cin>>n;
-   
-   while(n != 0){
+     do{
      cout<<"Informe o valor de n: ";
      cin>>n;
 
      if(n > maior){
        maior = n;
      }
-   }
 
-   cout<<"Maior numero: "<<maior;
+     }while(n != 0);
+
+   cout<<"\nMaior numero: "<<maior;
+   return 0;
 }
